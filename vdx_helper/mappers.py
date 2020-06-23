bda5a7919e2434baad8032d6eebaa1a151fbdeda
Tiago Santos <tiago.santos@vizidox.com>
2020-06-23 15:08:01 +0100

    Add return mapping for permission mapper
    VDX-301

--- vdx_helper/mappers.py
+++ vdx_helper/mappers.py
@@ -1,8 +1,10 @@
+from typing import List
+
 from vdx_helper.typing import Json
 from vdx_helper.models import EnginePermissionsView
 
 
-def permissions_mapper(json: Json):
+def permissions_mapper(json: Json) -> List[EnginePermissionsView]:
     permission_views = list()
     for json_permission in json:
         permission = EnginePermissionsView(
