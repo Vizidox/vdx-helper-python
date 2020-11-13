3a7b7079a6fab36ff5e612f7dc43cc193cea9eed
Rita Mariquitos <rita.mariquitos@morphotech.co.uk>
2020-11-13 18:42:00 +0000

    Add more json responses
    VDX-825

--- tests/json_responses.py
+++ tests/json_responses.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 from uuid import UUID
 
-from vdx_helper.models import FileView, EnginePermissionsView, CredentialView, PaginatedView
+from vdx_helper.models import FileView, EnginePermissionsView, CredentialView, PaginatedView, JobView, PartnerView, \
+    JobStatus, VerificationResponseView, VerificationStepResult, StepStatus, CertificateView, ClaimView, \
+    VerificationReport, VerificationStatus
 
 engine_json = [
     {
@@ -55,4 +57,120 @@ paginated_credential = {
     "items": [credential_json]
 }
 
-mapped_paginated_credential = PaginatedView(page=1, total_pages=1, per_page=20, total_items=1, items=[mapped_credential])
+mapped_paginated_credential = PaginatedView(page=1, total_pages=1, per_page=20, total_items=1,
+                                            items=[mapped_credential])
+
+partner_json = {
+    "id": "partner_id",
+    "name": "partner"
+}
+job_json = {
+    "uid": "123e4567-e89b-12d3-a456-426655440000",
+    "partner": partner_json,
+    "chain": "dogecoin",
+    "tags": [
+        "tag1"
+    ],
+    "status": 5,
+    "created_date": "2020-11-13T11:30:00.790190+00:00",
+    "scheduled_date": "2020-11-13T12:00:00+00:00"
+}
+
+paginated_job = {
+    "page": "1",
+    "total_pages": "1",
+    "per_page": "20",
+    "total_items": "1",
+    "items": [job_json]
+}
+
+mapped_partner = PartnerView(id="partner_id", name="partner")
+mapped_job = JobView(uid=UUID("123e4567-e89b-12d3-a456-426655440000"),
+                     partner=mapped_partner,
+                     chain='dogecoin',
+                     tags=["tag1"],
+                     status=JobStatus.scheduled,
+                     created_date=datetime.fromisoformat("2020-11-13T11:30:00.790190+00:00"),
+                     scheduled_date=datetime.fromisoformat("2020-11-13T12:00:00+00:00"),
+                     start_date=None,
+                     issued_date=None,
+                     finished_date=None,
+                     failed_date=None
+                     )
+
+mapped_paginated_job = PaginatedView(page=1, total_pages=1, per_page=20, total_items=1,
+                                     items=[mapped_job])
+
+verification_response_json = {
+    "verification": [
+        {
+            "name": "Checking certificate integrity",
+            "description": {
+                "hash_function": "SHA256",
+                "actual_hash": "d08f0971320c1cb70dcff0f5356bf8cd90ce4d03b4e13b9c82137c4ab1a3e659",
+                "expected_hash": "d08f0971320c1cb70dcff0f5356bf8cd90ce4d03b4e13b9c82137c4ab1a3e659"
+            },
+            "status": 1
+        },
+        {
+            "name": "Checking revocation date",
+            "description": {
+                "revocation_address": "myWUoZBBiMyBLHWKtNayfgduUWFed6bzCi",
+                "revocation_address_url": "https://chain.so/address/BTCTEST/myWUoZBBiMyBLHWKtNayfgduUWFed6bzCi",
+                "revocation_date": None
+            },
+            "status": 3
+        }
+    ]
+}
+mapped_verification_step_1 = VerificationStepResult(name='Checking certificate integrity',
+                                                    description={
+                                                        "hash_function": "SHA256",
+                                                        "actual_hash": "d08f0971320c1cb70dcff0f5356bf8cd90ce4d03b4e13b9c82137c4ab1a3e659",
+                                                        "expected_hash": "d08f0971320c1cb70dcff0f5356bf8cd90ce4d03b4e13b9c82137c4ab1a3e659"
+                                                    },
+                                                    status=StepStatus.passed)
+
+mapped_verification_step_2 = VerificationStepResult(name='Checking revocation date',
+                                                    description={
+                                                        "revocation_address": "myWUoZBBiMyBLHWKtNayfgduUWFed6bzCi",
+                                                        "revocation_address_url": "https://chain.so/address/BTCTEST/myWUoZBBiMyBLHWKtNayfgduUWFed6bzCi",
+                                                        "revocation_date": None
+                                                    },
+                                                    status=StepStatus.failed)
+
+mapped_verification = VerificationResponseView([mapped_verification_step_1, mapped_verification_step_2])
+
+certificate_json = {
+    "certificate": {
+        "uid": "123e4567-e89b-12d3-a456-426655440000",
+        "partner": partner_json,
+        "credential": credential_json,
+        "issued_date": "2020-02-11T15:34:05.813217+00:00",
+        "signature": "signature"
+    },
+    "last_verification": {
+        "status": 5,
+        "timestamp": "2020-02-11T15:34:05.813289+00:00"
+    }
+}
+
+paginated_certificate = {
+    "page": "1",
+    "total_pages": "1",
+    "per_page": "20",
+    "total_items": "1",
+    "items": [certificate_json]
+}
+
+mapped_claim = ClaimView(uid=UUID("123e4567-e89b-12d3-a456-426655440000"),
+                         partner=mapped_partner,
+                         credential=mapped_credential,
+                         issued_date=datetime.fromisoformat("2020-02-11T15:34:05.813217+00:00"),
+                         signature='signature')
+mapped_verification_report = VerificationReport(status=VerificationStatus.ok,
+                                                timestamp=datetime.fromisoformat("2020-02-11T15:34:05.813289+00:00"))
+mapped_certificate = CertificateView(certificate=mapped_claim, last_verification=mapped_verification_report)
+
+mapped_paginated_certificate = PaginatedView(page=1, total_pages=1, per_page=20, total_items=1,
+                                             items=[mapped_certificate])
