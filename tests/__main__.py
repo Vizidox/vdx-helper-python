e58e2d11f5deff36b268eab4323b91e31aa0b4ee
Rita Mariquitos <rita.mariquitos@morphotech.co.uk>
2020-11-25 18:32:26 +0000

    Complete testrunner
    VDX-751

--- tests/__main__.py
+++ tests/__main__.py
@@ -2,8 +2,9 @@ import unittest
 import coverage
 import os
 
+include = ["vdx_helper/*"]
 omitted = ['tests/*', 'vdx_helper/__init__.py']
 
 os.system('coverage erase')
 os.system('coverage run tests/_test_runner.py')
-os.system('coverage report -m --omit={}'.format(','.join(omitted)))
+os.system('coverage report -m --include={} --omit={}'.format(','.join(include), ','.join(omitted)))
