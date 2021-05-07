f8c93920acef867cb1b5aef6d3a391e3162fbe63
Joana Teixeira <joana.teixeira@vizidox.com>
2021-05-07 11:12:07 +0100

    Correct certificate mapper to not map None verifications

--- vdx_helper/mappers.py
+++ vdx_helper/mappers.py
@@ -113,9 +113,10 @@ def claim_mapper(json_: Json) -> ClaimView:
 
 
 def certificate_mapper(json_: Json) -> CertificateView:
+    json_verification = json_.get("last_verification")
     return CertificateView(
         certificate=claim_mapper(json_["certificate"]),
-        last_verification=verification_report_mapper(json_.get("last_verification"))
+        last_verification=verification_report_mapper(json_verification) if json_verification is not None else None
     )
 
 
